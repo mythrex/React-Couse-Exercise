e542d268e985500b7521d7a4b808c549d359b3ae
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-01-18 22:08:51 +0530

    Create a functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>I am a react App</h1>
+				<Person />
+			</div>
+			// React.createElement(
+			// 	'div',
+			// 	{ className: 'App' },
+			// 	React.createElement('h1', null, 'I am a react app!')
+			// )
+		);
+	}
 }
 
 export default App;
