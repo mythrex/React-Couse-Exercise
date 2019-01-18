a39fbc04d3417d19a321f3be641a896dac62bfbc
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-01-18 22:31:36 +0530

    Add state and props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-
 class App extends Component {
+	state = {
+		persons: [{ name: 'Shivam', age: 22 }]
+	};
 	render() {
 		return (
 			<div className="App">
 				<h1>I am a react App</h1>
-				<Person />
+				<button>Switch Name</button>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+				>
+					My Hobbies is playing.
+				</Person>
 			</div>
 			// React.createElement(
 			// 	'div',
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const person = () => {
-	return <p>I am a person</p>;
+const person = props => {
+	return (
+		<div>
+			<p>
+				I am {props.name} and I am {props.age} years old.
+			</p>
+			<p>{props.children}</p>
+		</div>
+	);
 };
 
 export default person;
