8efe00a6d892eedd184879eea529d1772664b473
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-01-19 14:36:10 +0530

    Two way binding

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ class App extends Component {
 	state = {
 		persons: [{ name: 'Shivam', age: 22 }]
 	};
-	switchNameHandler = () => {
-		console.log(this.state);
+	switchNameHandler = (newName, age) => {
+		this.setState({ persons: [{ name: newName, age: age }] });
+	};
+	nameChangeHandler = event => {
+		this.setState({ persons: [{ name: event.target.value, age: 22 }] });
 	};
 	render() {
 		return (
 			<div className="App">
 				<h1>I am a react App</h1>
-				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<button onClick={this.switchNameHandler.bind(this, 'sivam', 11)}>
+					Switch Name
+				</button>
 				<Person
 					name={this.state.persons[0].name}
 					age={this.state.persons[0].age}
+					click={this.switchNameHandler}
+					change={this.nameChangeHandler}
 				>
 					My Hobbies is playing.
 				</Person>
