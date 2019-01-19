826cfac9a9fba0c3c369f588c675acce2bc9d736
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-01-19 17:12:56 +0530

    Add Conditional Dynamically

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Person from './Person/Person';
 class App extends Component {
 	state = {
-		persons: [{ name: 'Shivam', age: 22 }]
+		persons: [{ name: 'Shivam', age: 22 }],
+		showPersons: false
 	};
 	switchNameHandler = (newName, age) => {
 		this.setState({ persons: [{ name: newName, age: age }] });
@@ -11,21 +12,27 @@ class App extends Component {
 	nameChangeHandler = event => {
 		this.setState({ persons: [{ name: event.target.value, age: 22 }] });
 	};
+	togglePersonHandler = () => {
+		const doesShow = this.state.showPersons;
+		this.setState({ showPersons: !doesShow });
+	};
 	render() {
 		return (
 			<div className="App">
 				<h1>I am a react App</h1>
-				<button onClick={this.switchNameHandler.bind(this, 'sivam', 11)}>
-					Switch Name
-				</button>
-				<Person
-					name={this.state.persons[0].name}
-					age={this.state.persons[0].age}
-					click={this.switchNameHandler}
-					change={this.nameChangeHandler}
-				>
-					My Hobbies is playing.
-				</Person>
+				<button onClick={this.togglePersonHandler}>Toggle Person</button>
+				<div>
+					{this.state.showPersons ? (
+						<Person
+							name={this.state.persons[0].name}
+							age={this.state.persons[0].age}
+							click={this.switchNameHandler}
+							change={this.nameChangeHandler}
+						>
+							My Hobbies is playing.
+						</Person>
+					) : null}
+				</div>
 			</div>
 			// React.createElement(
 			// 	'div',
