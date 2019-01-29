415fbe0fab4acd585847cbfb41410d2ed6f56528
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-01-29 12:39:41 +0530

    Add modularity

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+class Person extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<p onClick={this.props.click}>
+					I am {this.props.name} and I am {this.props.age} years old.
+				</p>
+				<p>{this.props.children}</p>
+				<input
+					type="text"
+					onChange={this.props.change}
+					value={this.props.name}
+				/>
+			</div>
+		);
+	}
+}
 
-const person = props => {
-	return (
-		<div>
-			<p onClick={props.click}>
-				I am {props.name} and I am {props.age} years old.
-			</p>
-			<p>{props.children}</p>
-			<input type="text" onChange={props.change} value={props.name} />
-		</div>
-	);
-};
-
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = props =>
-	props.persons.map((person, index) => {
-		return (
-			<Person
-				name={person.name}
-				age={person.age}
-				click={() => {
-					props.click(index);
-				}}
-				key={person.id}
-				change={event => {
-					props.change(event, person.id);
-				}}
-			/>
-		);
-	});
+class Persons extends Component {
+	state = {};
+	render() {
+		return this.props.persons.map((person, index) => {
+			return (
+				<Person
+					name={person.name}
+					age={person.age}
+					click={() => {
+						this.props.click(index);
+					}}
+					key={person.id}
+					change={event => {
+						this.props.change(event, person.id);
+					}}
+				/>
+			);
+		});
+	}
+}
 
 export default Persons;
